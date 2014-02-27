a53ed994119cdda144d2b78cb2ba766c5a270d3e
ningjiang <ning.n.jiang@gmail.com>
2014-02-27 14:01:51 +0800

    update gitree.c to v2
    V2: Redesign the core algorithm:
    1. Check valid git tree through key git files.
    2. For git tree, perform conformance check, give warnings when
       1) files break Git repo layout rule,
       2) git dirs name not terminated with .git,
       3) git dirs non-bare git tree,
       4) files not in a git tree.
    3. For non git tree, print all the files under it and then
       continue the check with its sub directories.
    
    Signed-off-by: ningjiang <ning.n.jiang@gmail.com>

--- gitree.c
+++ gitree.c
@@ -11,6 +11,16 @@
  *    warning "name not terminated with .git".
  * 2. When checking a .git tree name not terminated with .git,
  *    it will report the files not in a git tree by mistake.
+ *
+ * V2: Redesign the core algorithm:
+ * 1. Check valid git tree through key git files.
+ * 2. For git tree, perform conformance check, give warnings when
+ *    1) files break Git repo layout rule,
+ *    2) git dirs name not terminated with .git,
+ *    3) git dirs non-bare git tree,
+ *    4) files not in a git tree.
+ * 3. For non git tree, print all the files under it and then
+ *    continue the check with its sub directories.
  */
 #include <stdio.h>
 #include <sys/types.h>
@@ -20,6 +30,7 @@
 #include <unistd.h>
 
 #define SUBDIRNO 4096
+#define SUBFILENO 4096
 
 static char git_files[][128] = {
 	/* git files */
@@ -34,6 +45,7 @@ static char git_files[][128] = {
 	"MERGE_HEAD",
 	"MERGE_MODE",
 	"MERGE_MSG",
+	"MERGE_RR",
 	"RENAMED-REF",
 	"gitk.cache",
 	/* git dirs */
@@ -70,42 +82,75 @@ static char git_files[][128] = {
 	"temp.patch",
 };
 
-static char git_tree_nonbare[][128] = {
-	/* Allowed nonbare git trees */
-	"manifests",
-	"repo",
+static char exception_list[][128] = {
+	"/git/android/.repo",
 };
 
 static int git_files_array_size
 	= sizeof(git_files) / sizeof(git_files[0]);
-static int git_tree_nonbare_array_size
-	= sizeof(git_tree_nonbare) / sizeof(git_tree_nonbare[0]);
-static int sum, sum2;
-static int stage1, stage2, stage3;
+static int exception_list_array_size
+	= sizeof(exception_list) / sizeof(exception_list[0]);
+static int sum_break_layout_rule, sum_dir_name_not_with_git,
+	   sum_non_bare_git, sum_not_in_git;
 
 static void usage(void)
 {
-	fprintf(stderr, "Usage:\n"
-			"./gitree -1 pathname\n"
-			"./gitree -2 pathname\n"
-			"./gitree -3 pathname\n"
-			"1 - Perform Git repository layout conformance check\n"
-			"2 - Search for all non-bare Git trees\n"
-			"3 - Find all files not in a bare Git tree\n"
-			"Please perform the check in 1, 2, 3 order\n");
+	fprintf(stderr, "Usage: ./gitree pathname\n"
+			"Perform conformance check, give warnings when\n"
+			"1. files break Git repo layout rule\n"
+			"2. git dirs name not terminated with .git\n"
+			"3. git dirs non-bare git tree\n"
+			"4. files not in a git tree\n");
 	exit(-1);
+}
+
+static int in_exception_list(char *dirname)
+{
+	int i, str_len;
+
+	for (i = 0; i < exception_list_array_size; i++) {
+		str_len = strlen(exception_list[i]);
+		if (!strncmp(dirname, exception_list[i], str_len))
+			break;
+	}
+	if (i == exception_list_array_size)
+		return 0;
+	else
+		return 1;
 }
 
 static void check_gitree(char *dirname)
 {
+	char *last_dir;
+	int dir_name_with_git = 0;
+	int dir_len;
 	DIR *dirp;
 	struct dirent *direntp;
 	int i;
 
-	if (!stage1)
-		return ;
+	last_dir = strrchr(dirname, '/');
+	if (last_dir == NULL)
+		last_dir = dirname;
+	else
+		last_dir++;
 
-	printf("Checking %s\n", dirname);
+	dir_len = strlen(last_dir);
+
+	if ((dir_len >= 4) && !strncmp(last_dir + dir_len - 4, ".git", 4))
+		dir_name_with_git = 1;
+
+	if ((dir_name_with_git == 1) && (dir_len == 4)) {
+		if (!in_exception_list(dirname)) {
+			sum_non_bare_git++;
+			printf("WARNING: %s non-bare git tree\n", dirname);
+		}
+	}
+
+	if (!dir_name_with_git) {
+		sum_dir_name_not_with_git++;
+		printf("WARNING: %s name not terminated with .git\n",
+			dirname);
+	}
 
 	if ((dirp = opendir(dirname)) == NULL) {
 		fprintf(stderr, "check_gitree: opendir failed\n");
@@ -124,40 +169,13 @@ static void check_gitree(char *dirname)
 		if (i != git_files_array_size) {
 			continue;
 		} else {
-			sum++;
-			printf("WARNING: %s breaks Git repo layout rule\n",
-				direntp->d_name);
+			sum_break_layout_rule++;
+			printf("WARNING: %s/%s breaks Git repo layout rule\n",
+				dirname, direntp->d_name);
 		}
 	}
 
 	closedir(dirp);
-}
-
-static int in_nonbare_exp_list(char *dirname)
-{
-	int i;
-	char *last_dir;
-
-	last_dir = strrchr(dirname, '/');
-	last_dir++;
-
-	for (i = 0; i < git_tree_nonbare_array_size; i++) {
-		if (!strcmp(last_dir, git_tree_nonbare[i]))
-			break;
-	}
-	if (i == git_tree_nonbare_array_size)
-		return 0;
-	else
-		return 1;
-}
-
-static void check_gitree_nonbare(char *dirname)
-{
-	if (!stage2)
-		return ;
-
-	if (!in_nonbare_exp_list(dirname))
-		printf("%s\n", dirname);
 }
 
 static void gitree(char *dirname)
@@ -171,13 +189,17 @@ static void gitree(char *dirname)
 	struct dirent *direntp;
 	char *subdir[SUBDIRNO];
 	int i = 0, subdirn, str_len, dir_len, subdir_len;
-	int git[SUBDIRNO], git_bare[SUBDIRNO];
-	int has_dir_objects = 0, has_dir_refs = 0, skip_subtree_check = 0;
+	int has_dir_objects = 0, has_dir_refs = 0;
+	int has_file_HEAD = 0;
+	char *subfile[SUBFILENO];
+	int j = 0, subfilen, subfile_len;
 
 	if ((dirp = opendir(dirname)) == NULL) {
 		fprintf(stderr, "gitree: opendir failed\n");
 		exit(-1);
 	}
+
+	printf("Checking %s\n", dirname);
 
 	dir_len = strlen(dirname);
 	while ((direntp = readdir(dirp)) != NULL) {
@@ -186,6 +208,11 @@ static void gitree(char *dirname)
 		else if (!strcmp(direntp->d_name, ".."))
 			continue;
 		if (direntp->d_type == DT_DIR) {
+			if (!strcmp(direntp->d_name, "objects"))
+				has_dir_objects = 1;
+			if (!strcmp(direntp->d_name, "refs"))
+				has_dir_refs = 1;
+
 			subdir_len = strlen(direntp->d_name);
 			str_len = dir_len + 1 + subdir_len;
 			subdir[i] = malloc(str_len + 1);
@@ -193,28 +220,24 @@ static void gitree(char *dirname)
 			strcat(subdir[i], "/");
 			strcat(subdir[i], direntp->d_name);
 			subdir[i][str_len] = '\0';
-			if (!strncmp(direntp->d_name + subdir_len - 4, ".git", 4)) {
-				git[i] = 1;
-				if (subdir_len == 4)
-					git_bare[i] = 0;
-				else
-					git_bare[i] = 1;
-			} else {
-				git[i] = 0;
-				git_bare[i] = 0;
-			}
-			if (!strcmp(direntp->d_name, "objects"))
-				has_dir_objects = 1;
-			if (!strcmp(direntp->d_name, "refs"))
-				has_dir_refs = 1;
 			i++;
 			if (i >= SUBDIRNO) {
 				fprintf(stderr, "ERROR: gitree: reach max dir num\n");
 				exit(-1);
 			}
 		} else if (direntp->d_type == DT_REG) {
-			if (stage3 && !in_nonbare_exp_list(dirname))
-				printf("%s/%s\n", dirname, direntp->d_name);
+			if (!strcmp(direntp->d_name, "HEAD"))
+				has_file_HEAD = 1;
+
+			subfile_len = strlen(direntp->d_name);
+			subfile[j] = malloc(subfile_len + 1);
+			strcpy(subfile[j], direntp->d_name);
+			subfile[j][subfile_len] = '\0';
+			j++;
+			if (j >= SUBFILENO) {
+				fprintf(stderr, "ERROR: gitree: reach max file num\n");
+				exit(-1);
+			}
 		} else if (direntp->d_type == DT_UNKNOWN) {
 			fprintf(stderr, "ERROR: gitree: unknown file type\n");
 			exit(-2);
@@ -223,53 +246,54 @@ static void gitree(char *dirname)
 
 	closedir(dirp);
 
-	if (has_dir_objects && has_dir_refs) {
-		check_gitree(dirname);
-		if (stage1) {
-			printf("WARNING: %s name not terminated with .git\n",
-				dirname);
-			sum2++;
-		}
-		skip_subtree_check = 1;
-	}
-
 	subdirn = i;
-	for (i = 0; i < subdirn; i++) {
-		if (!skip_subtree_check) {
-			if (git[i] == 1) {
-				check_gitree(subdir[i]);
-				if (git_bare[i] == 0)
-					check_gitree_nonbare(dirname);
-			} else {
-				if (!stage3 || !in_nonbare_exp_list(dirname))
-					gitree(subdir[i]);
+	subfilen = j;
+
+	if (has_dir_objects && has_dir_refs && has_file_HEAD) {
+		for (i = 0; i < subdirn; i++)
+			free(subdir[i]);
+		for (j = 0; j < subfilen; j++)
+			free(subfile[j]);
+		check_gitree(dirname);
+	} else {
+		for (j = 0; j < subfilen; j++) {
+			if (!in_exception_list(dirname)) {
+				sum_not_in_git++;
+				printf("WARNING: %s/%s not in a git tree\n",
+					dirname, subfile[j]);
 			}
+			free(subfile[j]);
 		}
-		free(subdir[i]);
+		for (i = 0; i < subdirn; i++) {
+			gitree(subdir[i]);
+			free(subdir[i]);
+		}
 	}
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int dir_len;
+
+	if (argc != 2)
 		usage();
 
-	if (!strncmp(argv[1], "-1", 2))
-		stage1 = 1;
-	else if (!strncmp(argv[1], "-2", 2))
-		stage2 = 1;
-	else if (!strncmp(argv[1], "-3", 2))
-		stage3 = 1;
-	else
-		usage();
+	dir_len = strlen(argv[1]);
+	dir_len--;
+	while (argv[1][dir_len] == '/') {
+		argv[1][dir_len] = '\0';
+		dir_len--;
+	}
 
-	gitree(argv[2]);
+	gitree(argv[1]);
 
-	if (stage1)
-		printf("\nCheck Result:\n"
-		       "%d files break Git repo layout rule\n"
-		       "%d dirs name not terminated with .git\n",
-		       sum, sum2);
+	printf("\nCheck Result:\n"
+	       "%d files break Git repo layout rule\n"
+	       "%d git dirs name not terminated with .git\n"
+	       "%d git dirs non-bare git tree\n"
+	       "%d files not in a git tree\n",
+	       sum_break_layout_rule, sum_dir_name_not_with_git,
+	       sum_non_bare_git, sum_not_in_git);
 
 	return 0;
 }
